db68537e3868175baf23e35fdd784e2d8fc6883e
Ashar Farooq <ashar744@gmail.com>
2021-08-05 23:41:30 -0400

    added a reset func

--- src/components/Terminate.js
+++ src/components/Terminate.js
@@ -4,12 +4,18 @@ import { Questions } from "../Utils/Questions.js";
 
 const Terminate = () => {
 
-    const { score } = useContext(GameContext);
+    const { score, setScore, setGame } = useContext(GameContext);
+
+    const reset = () => {
+        setScore(0);
+        setGame("Menu");
+    };
 
     return (
         <div className="terminate">
             <h1> Quiz Completed!</h1>
             <h3>{score} / {Questions.length}</h3>
+            <button onClick={reset}className="btn btn-primary"></button>
         </div>
     )
 };

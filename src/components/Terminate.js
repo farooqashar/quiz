014624e1849913ace4c0f26303207c5ad9fa62ee
Ashar Farooq <ashar744@gmail.com>
2021-08-05 23:40:04 -0400

    added basic score display

--- src/components/Terminate.js
+++ src/components/Terminate.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GameContext } from "../Utils/Context.js";
+import { Questions } from "../Utils/Questions.js";
 
 const Terminate = () => {
+
+    const { score } = useContext(GameContext);
+
     return (
-        <div>
-            T
+        <div className="terminate">
+            <h1> Quiz Completed!</h1>
+            <h3>{score} / {Questions.length}</h3>
         </div>
     )
 };

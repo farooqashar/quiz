192483b8cadfd4d368dc9f8ee2588517d36f9211
Ashar Farooq <ashar744@gmail.com>
2021-08-05 23:05:37 -0400

    added skeleton of questions

---
+++ src/Utils/Questions.js
@@ -0,0 +1,26 @@
+export const Questions = [
+      { 
+      prompt: "What does MIT stand for?",
+      optionA: "1",
+      optionB: "2",
+      optionC: "3",
+      optionD: "4",
+      answer: "optionA"
+      },
+        { 
+      prompt: "What does MIT not stand for?",
+      optionA: "1",
+      optionB: "2",
+      optionC: "3",
+      optionD: "4",
+      answer: "optionB"
+      },
+     { 
+      prompt: "What is the current month?",
+      optionA: "1",
+      optionB: "2",
+      optionC: "3",
+      optionD: "4",
+      answer: "optionD"
+      }
+];
